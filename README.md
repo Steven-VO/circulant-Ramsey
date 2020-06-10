b957fd37e92e4550ab38803f253aafb1b013c656
spvoverb <steven.vanoverberghe@ugent.be>
2020-06-10 20:34:42 +0200

    License

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # circulant-Ramsey
 Generator for circulant Ramsey graphs.
+
+Large Ramsey graphs can be found in graph6 format in the 'RamseyGraphs'-folder.
--- generators/genCyc.c
+++ generators/genCyc.c
@@ -1,3 +1,31 @@
+/**
+ * genCyc: a program to generate circulant Ramsey colorings for different kinds of avoided subgraphs.
+ * 
+ * Author: Steven Van Overberghe
+ * 
+ * ---------------------------------
+ * 
+ * Copyright (c) 2020
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "sets.c"
@@ -8,7 +36,7 @@
 
 //Search for one Ramsey graph vs generate all
 #ifndef SEARCH_SINGLE
-    #define SEARCH_SINGLE 0
+    #define SEARCH_SINGLE 1
 #endif
 
 //Stop after a number of consecutive zero-results
